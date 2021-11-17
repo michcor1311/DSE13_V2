6211e5360439e441339294880d4e2a79231ee3a0
Michele Corona <michele.corona@hotmail.com>
2021-11-16 21:13:31 -0600

    Login
    Ya se puede hacer login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">{{ __('Inicio Dashboard') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('DSE_13 CRM Para el control de ventas!') }}
                 </div>
             </div>
         </div>
